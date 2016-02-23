1e9c1818a9deb68cd19d5753973e701a37e69497
Ariel <jdecks.visible@outlook.com>
2016-02-23 11:00:52 -0600

    Terminé de agregar el código falta hacer pruebas y testear los métodos de lectura

---
+++ Tarea5/src/tarea5/Categoria.java
@@ -0,0 +1,38 @@
+
+package tarea5;
+
+public class Categoria {
+    private int categoria; 
+    private String nombre, rango; 
+    public Categoria (){
+    
+    }
+    
+    public int getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(int categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRango() {
+        return rango;
+    }
+
+    public void setRango(String rango) {
+        this.rango = rango;
+    }
+   
+    public String toString(){
+        return "Termina"; 
+    }
+}
--- Tarea5/src/tarea5/Tarea5.java
+++ Tarea5/src/tarea5/Tarea5.java
@@ -15,7 +15,14 @@ public class Tarea5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        CategoriaDAO cat = new CategoriaDAO(); 
+        Categoria categoria = new Categoria();
+        
+        categoria.setNombre("Mosca");
+        categoria.setRango("51 - 100 kg");
+        categoria.setCategoria(1);
+        
+        cat.delete(categoria);
     }
     
 }
